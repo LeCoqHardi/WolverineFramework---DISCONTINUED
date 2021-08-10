12289a39311e73d198d0580ba39375e0c31a0463
Théo <88515240+LeCoqHardi@users.noreply.github.com>
2021-08-10 23:46:34 +0200

    Create CheckForUpdates.sh

---
+++ CheckForUpdates.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "==============================================="
+echo "             Check for Updates                 "
+echo "==============================================="
+
+mkdir temp
+cd temp
+wget https://raw.githubusercontent.com/LeCoqHardi/WolverineFramework_FR/main/Wolverine.sh
+mv Wolverine.sh WolverineCompare.sh
+cd ..
+cp Wolverine.sh temp
+
+file1="temp/Wolverine.sh"
+file2="temp/WolverineCompare.sh"
+clear
+
+echo "==============================================="
+echo "             Check for Updates                 "
+echo "==============================================="
+
+
+if cmp -s "$file1" "$file2"; then
+    printf "No Updates Available."
+else
+    printf "An Update is Available, please re clone the repository."
+fi
+
+sudo rm -r temp
