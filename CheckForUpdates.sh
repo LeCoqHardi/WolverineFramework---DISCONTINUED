bbd93ac90ceb0fdbc48b27b2149148c9a0daa090
Théo <88515240+LeCoqHardi@users.noreply.github.com>
2021-08-12 12:27:07 +0200

    Update CheckForUpdates.sh

--- CheckForUpdates.sh
+++ CheckForUpdates.sh
@@ -24,16 +24,24 @@ if cmp -s "$file1" "$file2"; then
     printf "No Updates Available."
     sudo rm -r temp/
 else
-    printf "An Update is Available, please wait..."
-    echo ""
-    cd ..
-    sudo rm -r WolverineFramework_EN/
-    git clone https://github.com/LeCoqHardi/WolverineFramework_EN
-    cd WolverineFramework_EN/
-    echo ""
-    echo "Update complete."
-    chmod a+x *.sh && chmod a+x Scripts/*
-    $SHELL
+    printf "An update is available, do you want to install it ? [y/n]"
+    read rep
+    if [ $rep = n ]
+        then
+        exit
+    elif [ $rep = y ]
+        then
+        echo ""
+        echo "Updating, please wait..."
+        cd ..
+        sudo rm -r WolverineFramework_FR/
+        git clone https://github.com/LeCoqHardi/WolverineFramework_FR
+        cd WolverineFramework_FR/
+        echo ""
+        echo "Update complete."
+        chmod a+x *.sh && chmod a+x Scripts/*
+        $SHELL
+    fi
     
 fi
 
