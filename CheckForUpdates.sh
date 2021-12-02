ecc3f1600282843fecd9984a34c918ba299d5027
Théo GROT <88515240+LeCoqHardi@users.noreply.github.com>
2021-12-02 12:25:11 +0100

    Update CheckForUpdates.sh

--- CheckForUpdates.sh
+++ CheckForUpdates.sh
@@ -4,12 +4,12 @@ echo "==============================================="
 echo "             Check for Updates                 "
 echo "==============================================="
 
-mkdir temp
+sudo mkdir temp
 cd temp
-wget https://raw.githubusercontent.com/LeCoqHardi/WolverineFramework/main/Wolverine.sh
-mv /etc/WolverineFramework/temp/Wolverine.sh WolverineCompare.sh
+sudo wget https://raw.githubusercontent.com/LeCoqHardi/WolverineFramework/main/Wolverine.sh
+sudo mv /etc/WolverineFramework/temp/Wolverine.sh WolverineCompare.sh
 cd ..
-cp /etc/WolverineFramework/Wolverine.sh temp
+sudo cp /etc/WolverineFramework/Wolverine.sh temp
 
 file1="/etc/WolverineFramework/temp/Wolverine.sh"
 file2="/etc/WolverineFramework/temp/WolverineCompare.sh"
@@ -35,11 +35,11 @@ else
         echo "Updating, please wait..."
         cd ..
         sudo rm -r WolverineFramework/
-        git clone https://github.com/LeCoqHardi/WolverineFramework
+        sudo git clone https://github.com/LeCoqHardi/WolverineFramework
         cd WolverineFramework/
         echo ""
         echo "Update complete."
-        chmod a+x *.sh && chmod a+x Scripts/*
+        sudo chmod a+x *.sh && chmod a+x Scripts/*
         $SHELL
     fi
     
